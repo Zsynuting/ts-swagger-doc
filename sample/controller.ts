0073fdf288fa335534a9a2ed8d8fb5142c1c043c
shaoyingzhu <shaoyingzhu@tencent.com>
2022-04-28 17:54:25 +0800

    update: ts-openapi version

--- sample/controller.ts
+++ sample/controller.ts
@@ -18,16 +18,16 @@ const example = {
 
 @SwaggerController('/test', '测试')
 export class TestController {
-  // @SwaggerAction('/test-action', 'post', '测试action', '测试action描述')
-  // @SwaggerActionResult({ name: '123', value: [1, 2, 3] })
-  // @SwaggerActionResult({ code: 0, message: 'error' }, 400)
-  // testAction(
-  //   @SwaggerActionParam('id', 'params', 'id描述', true, 123) id: string,
-  //   @SwaggerActionParam('name', 'query', 'name描述', false, 'z') name: string,
-  //   @SwaggerActionParam('model', 'body', 'model描述', true, example) model: Model,
-  // ): Model {
-  //   return { name: '123', value: '123' };
-  // }
+  @SwaggerAction('/test-action', 'post', '测试action', '测试action描述')
+  @SwaggerActionResult({ name: '123', value: [1, 2, 3] })
+  @SwaggerActionResult({ code: 0, message: 'error' }, 400)
+  testAction(
+    @SwaggerActionParam('id', 'params', 'id描述', true, 123) id: string,
+    @SwaggerActionParam('name', 'query', 'name描述', false, 'z') name: string,
+    @SwaggerActionParam('model', 'body', 'model描述', true, example) model: Model,
+  ): Model {
+    return { name: '123', value: '123' };
+  }
 
   @SwaggerAction('/test-action-1', 'post', '测试action', '测试action描述')
   testAction1(
